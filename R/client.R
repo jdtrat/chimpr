adb873663731c6cfd6e0bd74587316febc4d48b9
Scott Chamberlain <myrmecocystus@gmail.com>
2020-11-02 10:53:07 -0800

    improve r6 docs

--- R/client.R
+++ R/client.R
@@ -1,29 +1,6 @@
 #' chimpr client
 #'
 #' @export
-#' @param dc (character) a data center. default: us7. required.
-#' @param key (character) API key. Can either pass in here,
-#' or set with `Sys.setenv()` or pass in to each function call.
-#' optional if already set as an environment variable
-#'
-#' @format NULL
-#' @usage NULL
-#'
-#' @details
-#' **Methods**
-#'
-#' - `ping(...)`
-#'    - ... Curl options passed to [crul::HttpClient]
-#'
-#' - `root(fields = NULL, exclude_fields = NULL, parse = TRUE)`
-#'    - fields (list/vector) A comma-separated list of fields to return.
-#'      Reference parameters of sub-objects with dot notation.
-#'    - exclude_fields (list/vector) A comma-separated list of fields to
-#'      exclude. Reference parameters of sub-objects with dot notation.
-#'    - parse (logical) Whether to parse to list (`FALSE`) or
-#'      data.frame (`TRUE`). Default: `TRUE`
-#'    - ... Curl options passed to [crul::HttpClient]
-#'
 #' @examples \dontrun{
 #' (x <- ChmpClient$new(dc = "us7", key = "<your key>"))
 #' (x <- ChmpClient$new())
@@ -35,15 +12,25 @@
 ChmpClient <- R6::R6Class(
   "ChmpClient",
   public = list(
+    #' @field dc (character) a data center
     dc = NULL,
+    #' @field key (character) an API key
     key = NULL,
 
+    #' @description Create a new `ChmpClient` object
+    #' @param dc (character) a data center. default: us7. required.
+    #' @param key (character) API key. Can either pass in here,
+    #' or set with `Sys.setenv()` or pass in to each function call.
+    #' optional if already set as an environment variable
     initialize = function(dc = "us7", key = NULL) {
       assert_is(dc, "character")
       self$dc <- dc
       self$key <- check_key(key)
     },
 
+    #' @description print method for `ChmpClient` objects
+    #' @param x self
+    #' @param ... ignored
     print = function(x, ...) {
       cat("<ChmpClient>", sep = "\n")
       cat(paste0("  data center: ", self$dc %||% ""), sep = "\n")
@@ -51,10 +38,22 @@ ChmpClient <- R6::R6Class(
         if (nzchar(self$key)) "<private>" else "not set!"), sep = "\n")
     },
 
+    #' @description A health check endpoint for the Mailchimp API
+    #' @param ... Curl options passed to [crul::verb-GET]
     ping = function(...) {
       chmp_parse(chmp_GET(self$dc, "ping", self$key, query = list(), ...), TRUE)
     },
 
+    #' @description The API root resource links to all other resources
+    #' available in the API. Calling the root directory also returns details
+    #' about the Mailchimp user account.
+    #' @param fields (list/vector) A comma-separated list of fields to return.
+    #' Reference parameters of sub-objects with dot notation.
+    #' @param exclude_fields (list/vector) A comma-separated list of fields to
+    #' exclude. Reference parameters of sub-objects with dot notation.
+    #' @param parse (logical) Whether to parse to list (`FALSE`) or
+    #' data.frame (`TRUE`). Default: `TRUE`
+    #' @param ... Curl options passed to [crul::verb-GET]
     root = function(fields = NULL, exclude_fields = NULL, parse = TRUE, ...) {
       assert_is(parse, "logical")
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
--- R/list.R
+++ R/list.R
@@ -1,22 +1,17 @@
 #' Lists class
 #'
 #' @export
-#' @param conn a connection object. see [ChmpClient]
-#' @param id a mailchimp list id. required
 #' @param fields (list/vector) A comma-separated list of fields to return.
 #' Reference parameters of sub-objects with dot notation.
 #' @param exclude_fields (list/vector) A comma-separated list of fields to
 #' exclude. Reference parameters of sub-objects with dot notation.
-#' @param month (character) a year/month character string of the year/month
-#' you want. optional
+#' @param subscriber_hash (character) a subscriber hash identifying a subscriber
 #' @param count (integer/numeric) The number of records to return.
 #' Default value is 10.
 #' @param offset (integer/numeric) The number of records from a collection
 #' to skip. Iterating over large collections with this parameter can be slow.
 #' Default value is 0.
-#' @format NULL
-#' @usage NULL
-#' @details xxx
+#' @template all
 #' @note not all routes are implemented thus far
 #' @examples \dontrun{
 #' (conn <- ChmpClient$new())
@@ -42,26 +37,37 @@
 ChmpList <- R6::R6Class(
   "ChmpLists",
   public = list(
+    #' @field conn (character) a connection object, see [ChmpClient]
     conn = NULL,
+    #' @field id (character) an id
     id = NULL,
 
+    #' @description Create a new `ChmpList` object
+    #' @param conn a connection object. see [ChmpClient]
+    #' @param id (character) a mailchimp list id. required
+    #' @return A new `ChmpList` object
     initialize = function(conn, id) {
       self$conn <- conn
       assert_is(id, "character")
       self$id <- id
     },
 
+    #' @description print method for `ChmpList` objects
+    #' @param x self
+    #' @param ... ignored
     print = function(x, ...) {
       cat("<lists>", sep = "\n")
       cat(paste0("  id: ", self$id %||% ""), sep = "\n")
     },
 
+    #' @description Get list info
     info = function(fields = NULL, exclude_fields = NULL,
         key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
       private$get(file.path("lists", self$id), args, parse, key, ...)
     },
 
+    #' @description List activity
     activity = function(fields = NULL, exclude_fields = NULL,
         key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -69,6 +75,8 @@ ChmpList <- R6::R6Class(
         key, ...)
     },
 
+    #' @description List abuse reports
+    #' @param report (character) a report id
     abuse_reports = function(report = NULL, fields = NULL,
       exclude_fields = NULL, key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -78,6 +86,7 @@ ChmpList <- R6::R6Class(
       private$get(path, args, parse, key, ...)
     },
 
+    #' @description List clients
     clients = function(fields = NULL, exclude_fields = NULL,
         key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -85,6 +94,9 @@ ChmpList <- R6::R6Class(
         key, ...)
     },
 
+    #' @description Growth history
+    #' @param month (character) a year/month character string of the year/month
+    #' you want. optional
     growth_history = function(month = NULL, fields = NULL,
       exclude_fields = NULL, count = 10, offset = 0, key = NULL,
       parse = TRUE, ...) {
@@ -100,6 +112,9 @@ ChmpList <- R6::R6Class(
       private$get(path, args, parse, key, ...)
     },
 
+    #' @description Interest categories
+    #' @param category_id (character) a category id 
+    #' @param type (character) a type
     interest_categories = function(category_id = NULL, fields = NULL,
       exclude_fields = NULL, count = 10, offset = 0, type = NULL, key = NULL,
       parse = TRUE, ...) {
@@ -115,6 +130,7 @@ ChmpList <- R6::R6Class(
       private$get(path, args, parse, key, ...)
     },
 
+    #' @description Locations
     locations = function(fields = NULL, exclude_fields = NULL,
         key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -122,6 +138,7 @@ ChmpList <- R6::R6Class(
         key, ...)
     },
 
+    #' @description Info for a specific list member
     member = function(subscriber_hash, fields = NULL, exclude_fields = NULL,
         key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -129,6 +146,7 @@ ChmpList <- R6::R6Class(
         args, parse, key, ...)
     },
 
+    #' @description Activity for a specific list member
     member_activity = function(subscriber_hash, fields = NULL,
       exclude_fields = NULL, key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -136,6 +154,7 @@ ChmpList <- R6::R6Class(
         "activity"), args, parse, key, ...)
     },
 
+    #' @description Goals for a specific list member
     member_goals = function(subscriber_hash, fields = NULL,
       exclude_fields = NULL, key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -143,6 +162,7 @@ ChmpList <- R6::R6Class(
         "goals"), args, parse, key, ...)
     },
 
+    #' @description Notes for a specific list member
     member_notes = function(subscriber_hash, fields = NULL,
       exclude_fields = NULL, key = NULL, parse = TRUE, ...) {
       args <- ct(list(fields = fields, exclude_fields = exclude_fields))
@@ -150,6 +170,18 @@ ChmpList <- R6::R6Class(
         "notes"), args, parse, key, ...)
     },
 
+    #' @description Members info
+    #' @param email_type an email type
+    #' @param status a status value
+    #' @param since_timestamp_opt time stamp since
+    #' @param before_timestamp_opt time stamp before
+    #' @param since_last_changed since last changed
+    #' @param before_last_changed before last changed
+    #' @param unique_email_id a unique email id
+    #' @param vip_only vip only?
+    #' @param interest_category_id an interest category id
+    #' @param interest_ids interest ids
+    #' @param interest_match an interest match
     members = function(fields = NULL, exclude_fields = NULL, count = 10,
       offset = 0, email_type = NULL, status = NULL, since_timestamp_opt = NULL,
       before_timestamp_opt = NULL, since_last_changed = NULL,
